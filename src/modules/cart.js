bca541f12a792951927a532013cb48e0339cebcc
TatianaKan <klanukt@gmail.com>
2021-12-22 16:04:54 +0300

    сумма, удаление элементов

--- src/modules/cart.js
+++ src/modules/cart.js
@@ -6,8 +6,11 @@ const cart = () => {
   const cartModal = document.querySelector('.cart');
 
   const closeModal = cartModal.querySelector('.cart-close');
+  
+  const cartTotal = cartModal.querySelector('.cart-total > span');
 
-  const goodsWrapper = document.querySelector('.goods')
+  const goodsWrapper = document.querySelector('.goods');
+  const cartWrapper = document.querySelector('.cart-wrapper')
 
   const openCart = () => {
     const cart = localStorage.getItem('cart') ?
@@ -16,6 +19,9 @@ const cart = () => {
     cartModal.style.display = 'flex';
 
     renderCart(cart)
+    cartTotal.textContent = cart.reduce((sum, goodItem) => {
+return sum += goodItem.price
+    }, 0)
   }
   const closeCart = () => {
     cartModal.style.display = '';
@@ -46,6 +52,30 @@ const cart = () => {
       localStorage.setItem('cart', JSON.stringify(cart))
       console.log(goodItem);
 
+    }
+  })
+
+  cartWrapper.addEventListener('click', (event)=> {
+    if (event.target.classList.contains('btn-primary')) {
+      const cart = localStorage.getItem('cart') ?
+      JSON.parse(localStorage.getItem('cart')) : [];
+
+      const card = event.target.closest('.card');
+      const key = card.dataset.key;
+
+      const index = cart.findIndex ((item)=> {
+        return item.id === +key
+      })
+
+      cart.splice(index, 1)
+
+      localStorage.setItem('cart', JSON.stringify(cart));
+
+      renderCart(cart);
+    cartTotal.textContent = cart.reduce((sum, goodItem) => {
+return sum += goodItem.price
+    }, 0);
+  
     }
   })
 }
