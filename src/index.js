90c537f41c4803ea2545c40e79def0b9019baf73
TatianaKan <klanukt@gmail.com>
2021-12-22 17:00:55 +0300

    filter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import cart from "./modules/cart";
 import load from "./modules/load";
 import search from "./modules/search"
 import catalog from "./modules/catalog"
-import filter from "../filter";
+import filter from "./modules/filter"
 
 
 cart();
